411dfd371d1cf9f3856318549bf1da0ed4a4fbfe
CheeseCake-8 <43648362+CheeseCake-8@users.noreply.github.com>
2019-01-08 17:21:02 +0100

    Fix
    added comments and decreased the spaces between rows

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from random import *
 from tkinter.messagebox import  askokcancel
 
 colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']        # Liste des couleurs disponibles
-finalList = [0, 0, 0, 0]        # Liste = La combinaison de couleurs à deviner
-currentList = [0, 0, 0, 0]
-cercleCoord = []        # Liste, de listes  = coordonnées des 4 cercles, d'un essai   
-nbClic = 0        # gestion du nombre de "clic gauche souris", utilisée pour sélectionner une couleur
+finalList = [0, 0, 0, 0]                                                  # Liste = La combinaison de couleurs à deviner
+currentList = [0, 0, 0, 0]                                                #Liste = Couleurs choisies par l joueur
+cercleCoord = []                                                          # Liste, de listes  = coordonnées des 4 cercles, d'un essai   
+nbClic = 0                                                                # gestion du nombre de "clic gauche souris", utilisée pour sélectionner une couleur
 num = 1
 jouer = False
-def aPropos(): 
+def aPropos():         #Création de la section "à propos"             
     msg =Toplevel() 
     Message(msg, width=200, aspect=200, justify=CENTER, 
         text ='''KingMind
@@ -27,18 +27,18 @@ Version Alpha 0.2''').pack(padx =15, pady =10)
 #def imageboite(): 
 #        plateau.create_image(200, 350, image=bkg)
 
-def presentation(): 
+def presentation():    #Création de la section "présentation"
     msg =Toplevel() 
     Message(msg, width =200, aspect =150, justify =CENTER, 
         text ='''----Presentation du jeu----
 Le but du jeu est de trouver la combinaison magique choisie par l'ordinateur.
 Selectionne parmis 6 couleurs puis l'ordinateur te dira, grace a des ronds rouges ou blancs la proximite avec la bonne combinaison.''').pack(padx =15, pady =10)
 
-def deroule(): 
+def deroule():         #Création de la section "déroule"
     msg =Toplevel() 
     Message(msg, width =200, aspect =100, justify =CENTER, 
         text ='''WIP''').pack(padx =15, pady =10)
-def menu(fenetre):
+def menu(fenetre):     #Création de la barre de menu avec ses boutons
     "Barre de menu"
     top=Menu(fenetre)
     fenetre.config(menu=top)
@@ -82,13 +82,13 @@ def color(event):
         for i in range (4):
                 if (cercleCoord [i][0] <= X <= cercleCoord [i][2]) and (cercleCoord [i][1] <= Y <= cercleCoord [i][3]):
                         plateau.create_oval (cercleCoord[i], fill = colorList[nbClic])
-                        currentList[i] = nbClic
+                        currentList[i] = nbClic       #permet la coloration des cercles
         nbClic = nbClic+1        
 def jouer():
         """ Gestion du jeu: le clic sur le bouton "essai", déclenche l'évaluation de la combinaison proposée"""
         # Mise en place du bouton "essai N°":
         """ C'est à partir de l'ACTION sur ce bouton qu'il faut continuer le MOTEUR DU JEU """
-        plateau.delete(ALL)
+        plateau.delete(ALL)     #supprime tout sur le plateau pour reinitialiser le plateau
         global num
         num = 1
         choiceColor(num)        # appelle la mise en place des cercles qui seront colorés par: color(event)
@@ -98,25 +98,23 @@ def jouer():
 
 def nouvelessai():
         global num
-        print(bool(jouer))
-        #if jouer == True: #and finalList[]==colorList[]:
         num = num+1
-        choiceColor(num)
+        choiceColor(num)    
         for i in range (4):
             print("Comparaison essai :")
             print(currentList[i] == finalList[i])
             
-def choiceColor(num):
+def choiceColor(num):      #mise en place des cercles colorés
         """ Mise en place des cercles de la proposition, avant leur coloration"""
         for i in range (4):
                 # mise en place cercles
-                cercle = plateau.create_oval ((i+1)*40-15, num*70-15, (i+1)*40+15, num*70+15, outline = "white")
+                cercle = plateau.create_oval ((i+1)*40-15, 35+num*35-15, (i+1)*40+15, 35+num*35+15, outline = "white")
                 
                 cercleCoord.insert (i, plateau.coords (cercle))        # Récupère les coordonnées des cercles
-def quitter(): 
-    reponse=askokcancel('KingMind',"Tu veux vraiment quitter ? On fait pas la belle ?") 
-    if reponse:
-        fenetre.quit() 
+def quitter():       #créé la fenêtre  pour quitter la partie
+        reponse=askokcancel('KingMind',"Tu veux vraiment quitter ? On fait pas la belle ?") 
+        if reponse:
+                fenetre.quit() 
                        
 ##### PROGRAMME PRINCIPAL #####
         
@@ -133,3 +131,4 @@ menu(fenetre)
 plateau.bind('<Button-1>',color)
 
 fenetre.mainloop()
+
