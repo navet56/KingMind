c7e77b64d5195896c2243e7b92ac346a9e1d342c
Navet56 <evandib@gmail.com>
2019-01-09 11:33:49 +0100

    Ajout du verrouillage du jeu lorsqu'il n'a pas lieu
    Avec @cyrioncentori

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ currentList = [0, 0, 0, 0]                                                # List
 cercleCoord = []                                                          # Liste, de listes  = coordonnées des 4 cercles, d'un essai   
 nbClic = 0                                                                # gestion du nombre de "clic gauche souris", utilisée pour sélectionner une couleur
 num = 1
+jouant = False
 def aPropos():         #Création de la section "à propos"             
     msg =Toplevel() 
     Message(msg, width=200, aspect=200, justify=CENTER, 
@@ -37,7 +38,6 @@ def deroule():         #Création de la section "déroule"
     msg =Toplevel() 
     Message(msg, width =200, aspect =100, justify =CENTER, 
         text ='''----Comment jouer à KingMind----
-
 Pour changer les couleurs, il faut cliquer sur les 4 cercles qui vous sont présentés.
 Pour voir si votre combinaison est la bonne, il faut cliquer sur le bouton essai en haut à gauche.
 A chaque essai, 4 points de couleur rouges ou blancs vous sont présentés :
@@ -52,7 +52,7 @@ def menu(fenetre):     #Création de la barre de menu avec ses boutons
     top.add_cascade(label='Jeu',menu=J,underline=0)#création de la cascade Jeu
     J.add_command(label='Nouvelle partie',command=jouer,underline=0)#création du bouton Nouvelle partie
     J.add_command(label='Essai',command=nouvelessai,underline=0)#etc
-    J.add_command(label='Montrer la combinaison',command=revele,underline=0)
+    J.add_command(label='Montrer la combinaison',command=victoire,underline=0)
     R=Menu(top)
     top.add_cascade(label='Regles',menu=R,underline=0)
     R.add_command(label='Presentation',command=presentation,underline=0)
@@ -70,11 +70,15 @@ def tirage():
                 # Tracé d'un rectangle et des cercles représentant la combinaison à rechercher
                 plateau.create_oval ((i+1)*40-15, 10, (i+1)*40+15, 40, outline = "black")
                 txt = plateau.create_text((i+1)*40, 25, text="?", font="Arial 16 ", fill="blue")
-def revele():
+def victoire():
         """ Pour révéler la suite recherchée... """ 
         for i in range (len(finalList)):
                 plateau.create_oval ((i+1)*40-15, 10, (i+1)*40+15, 40, fill = finalList[i])
+        plateau.create_text(400, 255, text="La partie est terminée !", font="Arial 16 ", fill="blue")
+        global jouant
+        jouant = False
 def color(event):
+    if jouant:
         """ Gestion de l'événement "Clic gauche", sur un cercle, pour sélectionner une couleur """
     
         global nbClic
@@ -97,33 +101,41 @@ def jouer():
         num = 1
         creerCercle(num)        # appelle la mise en place des cercles qui seront colorés par: color(event)
         tirage()
+        global jouant
+        jouant = True
 def nouvelessai():
+    print("Nouvel essai. Jouer :")
+    print(jouant)
+    if jouant == True:
         global num
         num = num+1
+        if num > 10:
+            victoire()
+        
         creerCercle(num)
         for i in range(4):
             plateau.create_oval ((i+6)*40-15, 35+(num - 1)*35-15, (i+6)*40+15, 35+(num - 1)*35+15, outline = "white")
         
         i = 0
-        confirm = 0
+        confirme = 0
         for j in range(4):
             if finalList.count(currentList[j]) >= 1:
                 plateau.create_oval ((i+6)*40-15, 35+(num - 1)*35-15, (i+6)*40+15, 35+(num - 1)*35+15, fill = "white")
                 if currentList[j] == finalList[j]:
                     plateau.create_oval ((i+6)*40-15, 35+(num - 1)*35-15, (i+6)*40+15, 35+(num - 1)*35+15, fill = "red")
-                    confirm = confirm + 1
+                    confirme = confirme + 1
 
                 i = i + 1
                     
-        if confirm == 4:
-            revele()
+        if confirme == 4:
+            victoire()
+    
                 
 def creerCercle(num):      #mise en place des cercles colorés
         """ Mise en place des cercles de la proposition, avant leur coloration"""
         for i in range (4):
                 # mise en place cercles
                 cercle = plateau.create_oval ((i+1)*40-15, 35+num*35-15, (i+1)*40+15, 35+num*35+15, outline = "white")
-                
                 cercleCoord.insert (i, plateau.coords (cercle))        # Récupère les coordonnées des cercles
 def quitter():       #créé la fenêtre  pour quitter la partie
         reponse=askokcancel('KingMind',"Tu veux vraiment quitter ? On fait pas la belle ?") 
