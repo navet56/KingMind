fe202836de3662f322eb1f97b7ff73362532ffdb
Navet56 <evandib@gmail.com>
2019-01-17 00:09:21 +0100

    Ajout d'un système d'indice plus simple et passage en 1.2

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from tkinter.messagebox import  askokcancel
 
 # Variables :
 
-colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']        # Liste des couleurs disponibles
-idColorList = [0, 1, 2, 3, 4, 5]										  # Liste qui assigne des chiffres aux couleurs
-finalList = [0, 0, 0, 0]                                                  # Liste de la combinaison de couleurs à deviner, généré par l'ordinateur
+colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple','white','grey']        # Liste des couleurs disponibles
+idColorList = [0, 1, 2, 3, 4, 5, 6, 7]										  # Liste qui assigne des chiffres aux couleurs
+finalList = [0,0,0,0]                                                  # Liste de la combinaison de couleurs à deviner, généré par l'ordinateur
 currentList = [0, 0, 0, 0]                                                # Liste des couleurs choisies par le joueur
 cercleCoord = []                                                          # Liste, de listes  = coordonnées des 4 cercles, d'un essai   
 nbClic = 0                                                                # gestion du nombre de "clic gauche souris", utilisée pour sélectionner une couleur
@@ -25,10 +25,9 @@ def aPropos():         #Création de la section "à propos"
 Par Evan Diberder, Mael Le Boulicaut, Kilian Buan
 Tk version 8.4
 Python 3.7
-License GNU GPL V3
-Version Alpha 1.1''').pack(padx =15, pady =10)
+Version 1.2''').pack(padx =15, pady =10)
 
-def imageboite(): 
+def imageboite():
     plateau.create_image(200, 350, image=bkg)
 def imageking():
 	plateau.create_image(150, 600, image=theking)
@@ -73,17 +72,18 @@ def menu(fenetre):     #Création de la barre de menu avec ses boutons
 def tirage():
         """ Tirage de la combinaison de couleur à deviner + Mise en forme: suite de 5 couleurs à deviner """
         # Tracé d'un rectangle et des cercles représentant la combinaison à rechercher
+        idColorList = [0, 1, 2, 3, 4, 5, 6, 7]										  # Liste qui assigne des chiffres aux couleurs
         plateau.create_rectangle(10, 58, 190, 92, outline='white',fill='black') #création du rectangle autour de des 4 ronds de la combinaison finale
         for i in range (4):
                 finalList[i] = choice(idColorList)        # tirage au sort d'une couleur dans la liste: idColorList
-                plateau.create_oval ((i+1)*40-15, 10, (i+1)*40+15, 40, outline = "black")
                 txt = plateau.create_text((i+1)*40, 76, text="?", font="Arial 16 ", fill="white")
+                idColorList.remove(finalList[i])
 def color(event):
     if jouant:
         """ Gestion de l'événement "Clic gauche", sur un cercle, pour sélectionner une couleur """
     
         global nbClic
-        if nbClic >= 6: nbClic = 0
+        if nbClic >= 8: nbClic = 0
         # position du pointeur de la souris:
         X = event.x
         Y = event.y
@@ -116,21 +116,20 @@ def nouvelessai():              #fonction du bouton Essai, qui créer 4 cercles 
         creerCercle(nbreEssai)
         indice()
 def indice():
-	i = 0 #on remet i à 0 pour pouvoir continuer à l'utiliser
-	confirme = 0 #on initialise confime
-	colorCount = [0, 0, 0, 0, 0, 0] #on créé la liste qui permet de compter les couleurs
-	for j in range(4):
-		colorCount[finalList[j]] = colorCount[finalList[j]] + 1 #on incremente le comptage de la finalList 4 fois
-	for j in range(4):
-		colorCount[currentList[j]] = colorCount[currentList[j]] - 1 #on baisse de -1 le comptage de la liste fait par le joueur
-		if finalList.count(currentList[j]) >= 1 and colorCount[currentList[j]] >= 0: #si la finalList au moins 1 couleurs pareil et que la colorList[currentList] est toujours positive :
-			plateau.create_oval ((i+6)*40-15, (nbreEssai-1)*35+70, (i+6)*40+15, (nbreEssai - 1)*35+100, fill = "white")#on place une boule blanche
-			if currentList[j] == finalList[j]: #si la couleur est au même endroit dans la liste que celle de l'ordinateur
-				plateau.create_oval ((i+6)*40-15, (nbreEssai-1)*35+70, (i+6)*40+15, (nbreEssai - 1)*35+100, fill = "red")#on place une boule rouge
-				confirme = confirme + 1#on incrémente confirme de 1
-			i = i + 1
-		if confirme == 4:#si confirme est de 4, c'est que les 4 couleurs sont égals
-			findujeu()#on lance la fonction de fin du jeu, de victoire
+        confirme = 0
+        couleurbonne = 0
+        j = 0
+        for i in range(4):
+            if currentList[i] in finalList:
+                if currentList[i] == finalList[i] :
+                    plateau.create_oval ((j+6)*40-15, (nbreEssai-1)*35+70, (j+6)*40+15, (nbreEssai - 1)*35+100, fill = "red")#on place une boule rouge
+                    confirme = confirme + 1#on incrémente confirme de 1
+                    j = j + 1
+                if currentList[i] in finalList and currentList[i] != finalList[i]:
+                    plateau.create_oval ((j+6)*40-15, (nbreEssai-1)*35+70, (j+6)*40+15, (nbreEssai - 1)*35+100, fill = "white")#on place une boule blanche
+                    j = j + 1
+            if confirme == 4:#si confirme est de 4, c'est que les 4 couleurs sont égals
+                findujeu()#on lance la fonction de fin du jeu, de victoire"
 def findujeu():
     """ Pour révéler la suite recherchée... """ 
     global jouant
@@ -142,8 +141,7 @@ def findujeu():
         plateau.create_text(150, 720, text="La partie est terminée.", font="Arial 16 ", fill="white") #on créé le texte qui dit fin de partie
 def creerCercle(nbreEssai):      #mise en place des cercles colorés
         """ Mise en place des cercles de la proposition, avant leur coloration"""
-        for i in range (4):
-                # mise en place cercles
+        for i in range (4):# mise en place des cerlces
                 cercle = plateau.create_oval ((i+1)*40-15, nbreEssai*35+70, (i+1)*40+15, nbreEssai*35+100, outline = "white")
                 cercleCoord.insert (i, plateau.coords (cercle))        # Récupère les coordonnées des cercles
 def quitter():       #créé la fenêtre  pour quitter la partie
@@ -154,7 +152,7 @@ def quitter():       #créé la fenêtre  pour quitter la partie
 # Lancement du programme :
         
 fenetre= Tk()
-fenetre.title('KingMind 1.1')
+fenetre.title('KingMind 1.2')
 plateau = Canvas(fenetre, height =750,width=400, bg='black' )
 plateau.pack(side =RIGHT, padx =0, pady =0)
 can=Canvas(fenetre,height=750,width=200, bg='black') #espace de gauche utilisé pour le bouton essai
